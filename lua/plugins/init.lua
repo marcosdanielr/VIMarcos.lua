2ac97775fb0b7e00d5bbb96dd91ff78b2e7dc502
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-16 02:53:04 -0300

    chore: disable eslint of js files

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -103,8 +103,8 @@ local plugins = {
       local lint = require("lint")
 
       lint.linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
+        -- javascript = { "prettier" },
+        typescript = { "prettier" },
         javascriptreact = { "eslint_d" },
         typescriptreact = { "eslint_d" },
         svelte = { "eslint_d" },
