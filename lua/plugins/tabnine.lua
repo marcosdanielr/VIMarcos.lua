19169cc511194625dede832800d78bc194bc64dc
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-08-27 03:04:23 -0300

    chore: tabnine config

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -1,1 +1,10 @@
-require("tabnine").setup({})
+require("tabnine").setup({
+	disable_auto_comment = true,
+	accept_keymap = "<C-Tab>",
+	dismiss_keymap = "<C-]>",
+	debounce_ms = 800,
+	suggestion_color = { gui = "#808080", cterm = 244 },
+	exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+	log_file_path = nil,
+	ignore_certificate_errors = false,
+})
