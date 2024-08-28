6e7c3838db44a24437e134188285e5f513e67816
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-08-27 23:44:13 -0300

    fix: toggleterm new instance

--- lua/keybinds/toggleterm.lua
+++ lua/keybinds/toggleterm.lua
@@ -1,3 +1,22 @@
-set_key("n", "<A-1>", ":ToggleTerm size=10 direction=horizontal<CR>", ns)
-set_key("n", "<A-2>", ":ToggleTerm size=40 direction=vertical<CR>", ns)
-set_key("n", "<A-3>", ":ToggleTerm direction=float<CR>", ns)
+local horizontal_term = nil
+local vertical_term = nil
+local float_term = nil
+
+set_key(
+	"n",
+	"<A-h>",
+	":lua if not horizontal_term then horizontal_term = require('toggleterm.terminal').Terminal:new({ direction = 'horizontal', size = 10 }) end horizontal_term:toggle()<CR>",
+	ns
+)
+set_key(
+	"n",
+	"<A-v>",
+	":lua if not vertical_term then vertical_term = require('toggleterm.terminal').Terminal:new({ direction = 'vertical', size = 40 }) end vertical_term:toggle()<CR>",
+	ns
+)
+set_key(
+	"n",
+	"<A-i>",
+	":lua if not float_term then float_term = require('toggleterm.terminal').Terminal:new({ direction = 'float' }) end float_term:toggle()<CR>",
+	ns
+)
