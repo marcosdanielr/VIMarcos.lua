8be49c89ea669daf0dbdfa80a5a1282fab77557a
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-08-27 23:49:42 -0300

    feat: add new telescope keybinds

--- lua/keybinds/telescope.lua
+++ lua/keybinds/telescope.lua
@@ -1,3 +1,4 @@
-set_key("n", "<C-F>", ":lua require('telescope.builtin').live_grep({grep_open_files=true}) <CR>", ns)
-set_key("n", "<C-A>", ":Telescope live_grep<CR>", ns)
-set_key("n", "<C-P>", ":Telescope find_files<CR>", ns)
+set_key("n", "<Leader>f", ":lua require('telescope.builtin').live_grep({grep_open_files=true}) <CR>", ns)
+set_key("n", "<Leader>a", ":Telescope live_grep<CR>", ns)
+set_key("n", "<Leader>p", ":Telescope find_files<CR>", ns)
+set_key("n", "<Leader>gs", ":Telescope git_status<CR>", ns)
