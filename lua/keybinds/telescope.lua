3b6bcda2a2ccc733ab2db2851e83d2ca095ffbeb
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-10-27 17:07:20 -0300

    fix: telescope keybinds

--- lua/keybinds/telescope.lua
+++ lua/keybinds/telescope.lua
@@ -1,4 +1,5 @@
-set_key("n", "<Leader>f", ":lua require('telescope.builtin').live_grep({grep_open_files=true}) <CR>", ns)
+set_key("n", "<Leader>b", ":lua require('telescope.builtin').live_grep({grep_open_files=true}) <CR>", ns)
+set_key("n", "<Leader>f", ":lua require('telescope.builtin').current_buffer_fuzzy_find() <CR>", ns)
 set_key("n", "<Leader>a", ":Telescope live_grep<CR>", ns)
 set_key("n", "<Leader>p", ":Telescope find_files<CR>", ns)
 set_key("n", "<Leader>gs", ":Telescope git_status<CR>", ns)
