61037d503dd3c78a9dad4e1956d7bf4c9f4d6e53
Marcos Roque <devmarcosdanielr@gmail.com>
2025-06-08 04:32:24 -0300

    feat: add neovide config

--- init.lua
+++ init.lua
@@ -1,3 +1,19 @@
+if vim.g.neovide then
+	vim.g.neovide_window_blurred = true
+	vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+
+	local alpha = function()
+		return string.format("%x", math.floor(255 * vim.g.transparency or 0.4))
+	end
+
+	vim.g.neovide_opacity = 0.0
+	vim.g.transparency = 0.8
+	vim.g.neovide_background_color = "#0f1117" .. alpha()
+
+	vim.g.neovide_floating_blur_amount_x = 7.0
+	vim.g.neovide_floating_blur_amount_y = 7.0
+end
+
 require("core.vim")
 require("core.globals")
 require("core.lazy")
