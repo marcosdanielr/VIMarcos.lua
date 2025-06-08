faa65ce07ac55c0eb6319320b3338846ec117961
Marcos Roque <devmarcosdanielr@gmail.com>
2025-06-08 03:49:08 -0300

    fix: resize keybind

--- lua/core/keybinds/vim.lua
+++ lua/core/keybinds/vim.lua
@@ -7,8 +7,8 @@ set_key("t", "<Esc>", "<C-\\><C-n><C-w><C-w>", ns)
 -- window
 set_key("n", "<S-A-k>", ":resize -2<CR>", ns)
 set_key("n", "<S-A-j>", ":resize +2<CR>", ns)
-set_key("n", "<S-A-h>", ":vertical resize -2<CR>", ns)
-set_key("n", "<S-A-l", ":vertical resize +2<CR>", ns)
+set_key("n", "<S-A-l>", ":vertical resize -2<CR>", ns)
+set_key("n", "<S-A-h>", ":vertical resize +2<CR>", ns)
 set_key("n", "<Leader>h", ":split<CR>", ns)
 set_key("n", "<Leader>v", ":vsplit<CR>", ns)
 
@@ -33,3 +33,18 @@ set_key("n", "<space><", "viw<ESC>a><esc>bi<<esc>ela", ns)
 
 set_key("n", "<A-k>", ":m .-2<CR>==", ns)
 set_key("n", "<A-j>", ":m .+1<CR>==", ns)
+
+-- zoom
+vim.g.neovide_scale_factor = 1.0
+
+vim.keymap.set("n", "<C-=>", function()
+	vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.1
+end)
+
+vim.keymap.set("n", "<C-->", function()
+	vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / 1.1
+end)
+
+vim.keymap.set("n", "<C-0>", function()
+	vim.g.neovide_scale_factor = 1.0
+end)
