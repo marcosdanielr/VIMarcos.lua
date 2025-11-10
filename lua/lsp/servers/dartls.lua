ae4e75895e6b3f427d5a1b89856bbe18626adb37
Marcos Roque <devmarcosdanielr@gmail.com>
2025-11-09 23:50:19 -0300

    fix: dart lsp

--- lua/lsp/servers/dartls.lua
+++ lua/lsp/servers/dartls.lua
@@ -3,7 +3,8 @@ local nvim_lsp = require("lspconfig")
 local capabilities = _G.lsp_capabilities or vim.lsp.protocol.make_client_capabilities()
 
 nvim_lsp.dartls.setup({
-	cmd = { "dart", "language-server", "--protocol=lsp" },
+	-- cmd = { "dart", "language-server", "--protocol=lsp" },
+	cmd = { "flutter", "analysis-server", "--lsp" },
 	filetypes = { "dart" },
 	root_dir = nvim_lsp.util.root_pattern("pubspec.yaml"),
 	init_options = {
